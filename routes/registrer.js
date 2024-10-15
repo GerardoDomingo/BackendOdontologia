0f72985aac12832d2348ed3fa812bc407d0a95f9
EmmanuelRM17 <emma041117@gmail.com>
2024-10-15 11:24:09 -0600

    add:bien

--- routes/registrer.js
+++ routes/registrer.js
@@ -10,7 +10,7 @@ const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: 'emma041117@gmail.com',
-        pass: 'jihv dofs azqn zqoi',
+        pass: 'zgjb boek cqbg mphx',
     },
 });
 
