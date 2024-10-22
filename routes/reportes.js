78bb86b6617ecc4a3b93a097c5872e4d99d536e9
EmmanuelRM17 <emma041117@gmail.com>
2024-10-22 02:33:43 -0600

    reportes

--- routes/reportes.js
+++ routes/reportes.js
@@ -23,5 +23,24 @@ router.get('/logs', async (req, res) => {
         res.status(200).json(results);
     });
 });
+// Endpoint para obtener información de un paciente por su ID
+router.get('/paciente/:id', (req, res) => {
+    const pacienteId = req.params.id;
+
+    const query = 'SELECT * FROM pacientes WHERE id = ?';
+    db.query(query, [pacienteId], (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: 'Error al obtener la información del paciente' });
+        }
+
+        if (results.length === 0) {
+            return res.status(404).json({ message: 'Paciente no encontrado' });
+        }
+
+        res.status(200).json(results[0]); // Retornamos solo la primera coincidencia
+    });
+});
+
+
 
 module.exports = router;
